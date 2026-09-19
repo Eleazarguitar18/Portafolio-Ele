9568693845a45905aa7a19299cf4563660b3487c
Eleazarguitar18 <eleazarguitar18@gmail.com>
2026-09-19 19:13:42 -0400

    fix:presentation

--- src/data/about.ts
+++ src/data/about.ts
@@ -3,11 +3,13 @@ export const aboutData = {
   name: "Eleazar Jhonny",
   lastName: "Cruz Mamani",
   role: "Full Stack Developer",
-  description: "Desarrollador Full Stack (Lic. en Informática, UMSA) especializado en construir sistemas web institucionales robustos, seguros y escalables. Combino el poder de Node.js y Laravel en el backend con la modernidad de React y Livewire en el frontend. Me apasiona transformar requerimientos complejos en soluciones digitales eficientes, entregando aplicaciones de alto rendimiento centradas en una excelente experiencia de usuario y código mantenible.",
+  description:
+    "Desarrollador Full Stack (Lic. en Informática, UMSA) especializado en construir sistemas web institucionales robustos, escalables. Combino el poder de Node.js y Laravel en el backend con la modernidad de React y Livewire en el frontend. Me apasiona transformar requerimientos complejos en soluciones digitales eficientes, entregando aplicaciones de alto rendimiento centradas en una excelente experiencia de usuario y código mantenible.",
   profileImage: "/images/perfil.jpg",
   socialLinks: {
     github: "https://github.com/Eleazarguitar18",
-    linkedin: "https://www.linkedin.com/in/eleazar-jhonny-cruz-mamani-973546200",
-    email: "mailto:eleazarguitar18@gmail.com"
-  }
+    linkedin:
+      "https://www.linkedin.com/in/eleazar-jhonny-cruz-mamani-973546200",
+    email: "mailto:eleazarguitar18@gmail.com",
+  },
 };
